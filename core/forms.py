5e70faea728d22720df398a465e0af5b82b18950
kko3ch <tomko3ch@gmail.com>
2023-10-17 15:37:17 +0300

    Setup form fields

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,8 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'email', 'course']
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control', 'id': 'nameid'}),
+            'email': forms.EmailInput(attrs={'class':'form-control', 'id': 'emailid'}),
+            'course': forms.TextInput(attrs={'class':'form-control', 'id': 'courseid'}),
+        }
