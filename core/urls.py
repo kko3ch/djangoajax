413febfd83434778d6006692b5f9e505aa92af34
kko3ch <tomko3ch@gmail.com>
2023-10-18 11:43:55 +0300

    Created ajax function to update table without page refresh

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('save/', views.save_data, name='save')
 ]
 
--- core/views.py
+++ core/views.py
@@ -2,9 +2,29 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .forms import StudentForm
 from .models import Student
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     form = StudentForm()
     student = Student.objects.all()
     context = {'form': form, 'student': student}
     return render(request, 'core/home.html', context)
+
+@csrf_exempt
+def save_data(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            course = request.POST['course']
+
+            s = Student(name=name, email=email, course=course)
+            s.save()
+            stu = Student.objects.values()
+            student_data = list(stu)
+
+            return JsonResponse({'status': 'Data Saved', 'student_data': student_data})
+        
+        else: 
+            return JsonResponse({'status': 'Unable To Save'})
